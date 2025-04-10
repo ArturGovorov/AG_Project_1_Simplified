a94d5713972ac34c35e903db8dbcc9e294f7098e
ArturGovorov <ArturGovorov@users.noreply.github.com>
2025-04-10 18:16:21 +0300

    Внес правки

--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -2,7 +2,6 @@ import httpx
 from jsonschema import validate
 
 from schemas.USER_DATA_SCHEMA import USER_DATA_SCHEMA
-from schemas.LIST_RESOURCE_SCHEMA import LIST_RESOURCE_SCHEMA
 
 BASE_URL = "https://reqres.in"
 LIST_USERS = "/api/users?page=2"
@@ -10,9 +9,6 @@ SINGLE_USER = "/api/users/2"
 USER_NOT_FOUND = "/api/users/23"
 EMAIL_ENDS = "@reqres.in"
 AVATAR_ENDS = "-image.jpg"
-LIST_RESOURCE = "/api/unknown"
-SINGLE_RESOURCE = "/api/unknown/2"
-RESOURCE_NOT_FOUND = "/api/unknown/23"
 
 
 def test_list_users():
@@ -37,40 +33,4 @@ def test_single_user():
 
 def test_user_not_found():
     response = httpx.get(BASE_URL + USER_NOT_FOUND)
-    assert response.status_code == 404
-
-
-def test_list_resource():
-    response = httpx.get(BASE_URL + LIST_RESOURCE)
-    assert response.status_code == 200
-    resource_list = response.json()['data']
-    ids = [item['id'] for item in resource_list]
-    assert len(ids) == len(set(ids))
-
-    for item in resource_list:
-        validate(item, LIST_RESOURCE_SCHEMA)
-        for key in LIST_RESOURCE_SCHEMA['required']:
-            assert key in item
-        assert 1900 <= item['year'] <= 2100
-        assert isinstance(item['id'], int)
-        assert isinstance(item['name'], str)
-        assert isinstance(item['year'], int)
-        assert isinstance(item['color'], str)
-        assert isinstance(item['pantone_value'], str)
-
-
-def test_single_resource():
-    response = httpx.get(BASE_URL + SINGLE_RESOURCE)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert 'data' in response_json
-    assert 'support' in response_json
-    support_info = response_json['support']
-    required_support_keys = ['url', 'text']
-    for key in required_support_keys:
-        assert key in support_info
-
-
-def test_resource_not_found():
-    response = httpx.get(BASE_URL + RESOURCE_NOT_FOUND)
     assert response.status_code == 404
